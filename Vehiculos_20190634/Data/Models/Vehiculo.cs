8fc1420e011a162ffd7d08590278dafb96b51461
humberto12345 <humbertogil19.hg@gmail.com>
2022-11-04 00:44:15 -0400

    Add files via upload

---
+++ Vehiculos_20190634/Data/Models/Vehiculo.cs
@@ -0,0 +1,11 @@
+
+namespace Vehiculos_20190634.Data.Models;
+
+public class Vehiculos
+{
+    public string? VehiculoID { get; set; }
+    public string? Marca { get; set; } 
+    public string? Modelo { get; set; }
+    public int Año { get; set; }
+    public string? Color { get; set; }
+}
